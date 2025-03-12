6f932c412d42373307834ff7db98ced7cc35e15e
Muhammad Abid <abidroid1@gmail.com>
2025-03-12 07:56:46 +0500

    fixes

--- pages/javascript/prototype-demo2.js
+++ pages/javascript/prototype-demo2.js
@@ -14,7 +14,17 @@ function Person(firstName, lastName, year){
     }
 }
 
+function Student(firstName, lastName, year) {
+// Call the Person constructor (like super in classes)
+Person.call(this, firstName, lastName, year);
+}
+
 const ali = new Person("M", "Ali", 1984);
 console.log(ali.getFullName());
 console.log(ali.getAge());
 
+console.log('******************************');
+
+const abid = new Student('Abid', "Ali", 1988);
+console.log(abid.getFullName());
+console.log(abid.getAge());
